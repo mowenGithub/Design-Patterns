9e7820acf808c12d3dfcbacc2c26868294410417
wenlong.mo <wenlong.mo@qunar.com>
2015-11-07 16:15:31 +0800

    搭起架子

---
+++ src/main/java/com/mowen/designpattern/Main.java
@@ -0,0 +1,10 @@
+package com.mowen.designpattern;
+
+/**
+ * Created by mowen on 15-11-7.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Learning Design Patterns");
+    }
+}
