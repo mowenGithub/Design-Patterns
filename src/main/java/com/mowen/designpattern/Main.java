4a9b7cffbb77919736f83545fa39490eba33fec0
wenlong.mo <wenlong.mo@qunar.com>
2015-11-09 17:48:59 +0800

    比重随机算法

--- src/main/java/com/mowen/designpattern/Main.java
+++ src/main/java/com/mowen/designpattern/Main.java
@@ -1,10 +1,36 @@
 package com.mowen.designpattern;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by mowen on 15-11-7.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Learning Design Patterns!");
+        List<Double> weights = new ArrayList<Double>();
+        weights.add(0.3);
+        weights.add(0.7);
+
+        System.out.println(weightedChoice(weights));
+    }
+
+    //比重随机值
+    private static int weightedChoice(List<Double> weights) {
+        Random random = new Random();
+        double total = 0;
+        int winner = 0;
+        for(int i = 0; i < weights.size(); ++i) {
+            double weight = weights.get(i).doubleValue();
+            total += weight;
+            if(random.nextDouble() * total < weight) {
+                winner = i;
+            }
+        }
+        if(random.nextDouble() <= total) {
+            return winner;
+        }
+        return -1;
     }
 }
