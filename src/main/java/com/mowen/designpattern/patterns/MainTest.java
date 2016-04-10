ca856dac712d4ea770d7ca1730777203d5475a2b
wenlong.mo <1245146613@qq.com>
2016-04-10 22:55:03 +0800

    第十三章 与设计模式相处

---
+++ src/main/java/com/mowen/designpattern/patterns/MainTest.java
@@ -0,0 +1,11 @@
+package com.mowen.designpattern.patterns;
+
+/**
+ * Created by mowen on 4/10/16.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("There is no codes");
+    }
+}
