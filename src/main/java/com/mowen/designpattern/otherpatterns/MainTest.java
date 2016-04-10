cc0ffddc52280e26fa1c06e7cfe098abfe7b7b20
wenlong.mo <1245146613@qq.com>
2016-04-11 00:57:27 +0800

    附录A 剩下的模式

---
+++ src/main/java/com/mowen/designpattern/otherpatterns/MainTest.java
@@ -0,0 +1,11 @@
+package com.mowen.designpattern.otherpatterns;
+
+/**
+ * Created by mowen on 4/10/16.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("这章有时间每种模式抽取出来单独成章");
+    }
+}
